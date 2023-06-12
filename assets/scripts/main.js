f9f094d71bf5dfc1c9e7b33b04aad0624395d192
Mystic_Coder <mysticcoder100@gmail.com>
2023-06-12 18:02:58 +0100

    Commit done after making some design for review by partner

---
+++ assets/scripts/main.js
@@ -0,0 +1,49 @@
+const yearElement = document.querySelector(".year");
+const fieldsElement = document.querySelector(".fields");
+const closeBtn = document.querySelectorAll("[data-close]");
+const openBtn = document.querySelectorAll("[data-open]");
+
+let fields = [
+	{
+		id: "firstname",
+		label: "First Name",
+		type: "text",
+	},
+	{
+		id: "lastname",
+		label: "Last Name",
+		type: "text",
+	},
+	{
+		id: "phonenumber",
+		label: "Phone Number",
+		type: "text",
+	},
+	{
+		id: "email",
+		label: "Email",
+		type: "email",
+	},
+];
+
+let year = new Date().getFullYear();
+yearElement.textContent = year;
+
+fields.forEach((field) => {
+	fieldsElement.innerHTML += `<div class="floating_input">
+    <input type="text" class="input" type="${field.type}" id="${field.id}" placeholder="a">
+    <label for="${field.id}" class="label">${field.label}</label>
+    </div>`;
+});
+
+let handleClose = function () {
+	let target = document.querySelector(this.dataset.target);
+	target.classList.remove("open");
+};
+let handleOpen = function () {
+	let target = document.querySelector(this.dataset.target);
+	target.classList.add("open");
+};
+
+closeBtn.forEach((btn) => btn.addEventListener("click", handleClose));
+openBtn.forEach((btn) => btn.addEventListener("click", handleOpen));
