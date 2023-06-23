264e79416381c859b51e22374c631929a5e9e6e1
Mystic_Coder <mysticcoder100@gmail.com>
2023-06-23 16:06:04 +0100

    Commit made after adding some styles like the font size becoming bigger on large screen, transitioning the sidebar links and some script by adding a timeout to the valMsgElement

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -1,4 +1,5 @@
-user_details.addEventListener("submit", () => {
+user_details.addEventListener("submit", (e) => {
+    e.preventDefault();
     let arr = []
     for (let i of [...fieldsElement.querySelectorAll("input")]){
         if (i.value.trim() === "") {
@@ -6,4 +7,6 @@ user_details.addEventListener("submit", () => {
             valMsgElement.innerHTML = `Please fill out the following fields: ${arr.join(", ")}`
         }
     }
+
+    setTimeout(() => valMsgElement.innerHTML = "", 2000);
 })
